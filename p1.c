9d9546b4cdaf1830e95f88ff9a465c1a59ebda30
Pranjal <pranjal.0902@yahoo.in>
2018-03-19 22:29:11 +0530

    Updated comments in two files p1.c and p2.c

--- p1.c
+++ p1.c
@@ -1,13 +1,26 @@
+/*This program is to SET, CLEAR, TOGGLE a particular bit*/
 #include <stdio.h>
 
-#define CLEAR(X,POS) (X & (~(1<<POS)))
+#define SET(X,POS) 		(X & (~(1<<POS)))
+#define CLEAR(X,POS)	(X | (1<<POS))
+#define CLEAR(X,POS) 	(X ^ (1<<POS))
 
-int main()
+int main(void)
 {
-    char ch = 7;
-    printf("before: %b\n",ch);
-    ch = CLEAR(ch, 1);
-    printf("after: %b\n",ch);
-
-    return 0;
+    char ch = 7;		//0111
+    printf("Orignal: %b\n",ch);	
+    
+	/*Clear a particular bit*/
+	ch = CLEAR(ch, 1);	//0110
+    printf("Clear bit 1: %b\n",ch);
+	
+	/*Set a particular bit*/
+	ch = SET(ch, 4);	//1110
+	printf("Clear bit 4: %b\n",ch);
+	
+	/*Toggle a particular bit*/
+	ch = TOGGLE(ch, 3);	//1010
+	printf("Toggle bit 2: %b\n",ch);
+    
+	return 0;
 }
