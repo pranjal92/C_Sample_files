8531fea3a8c9d67e0f1adf2b2f208d299953414b
Upadhyay Pranjal Lokeshchandra (RBEI/ECG7) <pranjal.0902@yahoo.in>
2018-03-20 14:56:26 +0530

    Modified file Struct_Copy_WO_Maclloc.c

--- Struct_Copy_WO_Maclloc.c
+++ Struct_Copy_WO_Maclloc.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 #include<string.h>
 
+void strupr(char *s)
+{
+	char i=0;
+	for (i=0;s[i]!='\0';i++)
+	{
+		s[i] = toupper(s[i]);
+	}
+}
+
 int main()
 {
     /* A structure to store name and age*/
@@ -16,7 +25,9 @@ int main()
     struct a e2 = e1;
     
     /*Changing name of struct e2.*/
-    e2.n = "Rahul";
+	strupr(e2.n);
+	//e2.n[0]='R';
+	//e2.n="Rohit";
     
     /*Printing name and address of e1 and e2*/
     printf("%s %u\n",e1.n, &e1);
